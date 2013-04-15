8f6fa1923a6a02170865aea349e69feec9dc181a
Adam Borocz <boroczadam@gmail.com>
2013-04-15 18:35:00 +0100

    Initial commit

---
+++ cookbook-development.gemspec
@@ -0,0 +1,16 @@
+Gem::Specification.new do |s|
+  s.name                   = 'cookbook-development'
+  s.version                = '0.1.0'
+  s.summary                = 'Just a way to abstract our Rake tasks for Chef Cookbooks'
+  s.require_path           = 'lib'
+  s.authors                = ['Adam Borocz']
+  s.email                  = ['adam@hipsnip.com']
+  s.platform               = Gem::Platform::RUBY
+  s.files                  = Dir.glob('lib/**/*.rake')
+  s.required_ruby_version  = '>= 1.9.3'
+
+  s.add_dependency "chef", "~> 11.4.0"
+  s.add_dependency 'foodcritic', '~> 2.0.0'
+  s.add_dependency "chefspec", "~> 1.0.0.rc1"
+  s.add_dependency "strainer", "~> 2.0.0"
+end
---
+++ lib/cookbook-development/tasks.rb
@@ -0,0 +1,69 @@
+require 'fileutils'
+
+namespace :cookbook do
+  # Paths to exclude from the bundle when publishing
+  EXCLUDE_PATHS = [
+    ".git", ".gitignore", ".rvmrc", ".ruby-version", ".travis.yml", ".kitchen.yml", "Gemfile",
+    "Gemfile.lock", "Berksfile", "Berksfile.lock", "Strainerfile", "spec", "test", "Rakefile"
+  ]
+
+  desc "Run Strainer to create sandbox and test cookbook"
+  task :test do
+    puts "=== Running Strainer... ==="
+    sh "bundle exec strainer test"
+  end
+
+  desc "Run Strainer with fail-fast - for development"
+  task :dev_test do
+    puts "=== Running Strainer... ==="
+    sh "bundle exec strainer test --fail-fast"
+  end
+
+  desc "Run Strainer and then Test Kitchen - won't work on Travis"
+  task :full_test do
+    Rake::Task["test"].invoke
+    puts "=== Running Test Kitchen... ==="
+    Rake::Task["kitchen:all"].invoke
+  end
+
+  desc "Bundle this cookbook for the Opscode Community site"
+  task :bundle do |t|
+    sh "knife cookbook metadata from file metadata.rb"
+    tarball_name = "#{COOKBOOK_NAME}.tar.gz"
+    temp_dir = File.join(Dir.tmpdir, "chef-upload-cookbooks")
+    temp_cookbook_dir = File.join(temp_dir, COOKBOOK_NAME)
+    tarball_dir = PACKAGE_DIR
+
+    FileUtils.rm_rf temp_dir if File.exists? temp_dir
+    FileUtils.mkdir temp_dir
+    FileUtils.mkdir temp_cookbook_dir
+
+    cookbook_files.each do |file|
+      FileUtils.cp_r(file, temp_cookbook_dir)
+    end
+
+    sh "tar -C #{temp_dir} -czvf #{File.join(tarball_dir, tarball_name)} #{COOKBOOK_NAME}"
+
+    FileUtils.rm_rf temp_dir
+  end
+end
+
+begin
+  require 'kitchen/rake_tasks'
+  Kitchen::RakeTasks.new
+rescue LoadError
+  puts ">>>>> Kitchen gem not loaded, omitting tasks" unless ENV['CI']
+end
+
+
+################################################################################
+
+def cookbook_files
+  Dir.glob("*").reject do |path|
+    if EXCLUDE_PATHS.include? File::basename(path)
+      true
+    else
+      false
+    end
+  end
+end
