af1702296945ac183d3434283484bcf135309650
Adam Borocz <boroczadam@gmail.com>
2013-06-06 15:41:24 +0100

    Fix issue with Gem LoadError not getting caught

--- lib/cookbook-development/tasks.rb
+++ lib/cookbook-development/tasks.rb
@@ -57,7 +57,7 @@ end
 begin
   require 'kitchen/rake_tasks'
   Kitchen::RakeTasks.new
-rescue
+rescue StandardError, LoadError
   puts ">>>>> Kitchen gem not loaded, omitting tasks" unless ENV['CI']
 end
 
